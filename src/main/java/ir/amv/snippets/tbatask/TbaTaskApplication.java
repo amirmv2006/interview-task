a322b345e7b74d921a132f0ebd97f1e9a06ba26f
Amir <amirmv2006@gmail.com>
2018-06-30 12:53:13 +0800

    Initial commit

---
+++ src/main/java/ir/amv/snippets/tbatask/TbaTaskApplication.java
@@ -0,0 +1,12 @@
+package ir.amv.snippets.tbatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbaTaskApplication.class, args);
+	}
+}
